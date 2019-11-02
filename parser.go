252a833fe4efc1be333c950aeaef707925c357e9
Vo Tuan <votuan921@gmail.com>
2019-11-02 15:17:22 +0700

    Add get Field Type

--- parser.go
+++ parser.go
@@ -4,11 +4,52 @@ import "fmt"
 import "go/token"
 import "go/parser"
 import "go/ast"
+import "bytes"
+import "bufio"
+import "os"
 
 const PARSER_FILE_PATH = "db.go"
 
-func  main()  {
+func readFileToString(path string) ([]string, error) {
+	file, err := os.Open(path)
+	
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+    return lines, scanner.Err()
+}
+
+func getStringFromNodePosition(fset *token.FileSet, fStr []string, pos token.Pos, end token.Pos) string {
+	linePos, colPos := fset.Position(pos).Line, fset.Position(pos).Column
+	lineEnd, colEnd := fset.Position(end).Line, fset.Position(end).Column
+	buf := bytes.Buffer{}
+
+	if linePos == lineEnd {
+		buf.WriteString(fStr[linePos-1][colPos-1:colEnd-1])
+	} else {
+		buf.WriteString(fStr[linePos-1][colPos-1:])
+		for i:= linePos+1; i < lineEnd; i++ {
+			buf.WriteString(fStr[i])
+		}
+		buf.WriteString(fStr[lineEnd-1][:colEnd-1])
+	}
+	
+	return buf.String()
+}
+
+func  main()  {
+	src, err := readFileToString(PARSER_FILE_PATH)
+
+	if err != nil {
+		fmt.Println("Reading file error ", err)
+	}
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, PARSER_FILE_PATH, nil, 0)
 
@@ -34,7 +75,8 @@ func  main()  {
 			fmt.Println("Struct: ", typeSpec.Name)
 			fields := structType.Fields.List
 			for _, field := range fields {
-				fmt.Printf("--Field name: %s\n", field.Names)
+				fmt.Println("--Field name: ", field.Names[0])
+				fmt.Println("----Type: ", getStringFromNodePosition(fset, src, field.Type.Pos(), field.Type.End()))
 			}
 		}
 	}
